c8fc89462f8d250e1fadde1469d4f3ccc1cba6e2
Geethanimal <geethanimal0@gmail.com>
2020-09-22 01:48:33 +0530

    quick update
    I just quick update few things of index.html web page. I changed the javascipt file and some new css codes for pop up modal.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-
-
-document.getElementById('btn_ca').addEventListener('click',function(){
+/*document.getElementById('btn_ca').addEventListener('click',function(){
 	document.querySelector('.bg-modal').style.display = 'flex';
-});
+});*/ 
+
+/*this code is not working*/
