bad07de672261668ff24bbad5355e41bcdc993db
Geethanimal <geethanimal0@gmail.com>
2021-03-25 09:04:13 +0530

    quick update
    quick update

--- README.md
+++
@@ -1,6 +0,0 @@
-# ED_platform
-This repository for E- education platform
-
-## subheader
-
-watch tutorial on YouTube.
--- index.js
+++
@@ -1,5 +0,0 @@
-/*document.getElementById('btn_ca').addEventListener('click',function(){
-	document.querySelector('.bg-modal').style.display = 'flex';
-});*/ 
-
-/*this code is not working*/
